60e5588c4d0e211a153e9aac88028f4d9889dd02
Aaron Santiago <me@aaron.work>
2025-08-16 23:37:47 -0400

    Initial working commit

--- main.js
+++ main.js
@@ -1,4 +1,72 @@
-import config from '3lib-config';
+import config from "3lib-config";
 
 config.init();
-console.log(config.get()["test"]);
+
+let db = {};
+let plugins = {
+  "core": {
+    "plugin": {
+      updateEntry: (config, runApi, ...args) => updateEntry(...args),
+      getEntry: (config, runApi, ...args) => getEntry(...args),
+    },
+    config: {},
+  }
+};
+
+async function initialize() {
+  await _loadPlugins();
+
+  await _runPlugins("onBeforeInitialize", db);
+  await _runPlugins("onInitialize", db);
+  await _runPlugins("onAfterInitialize", db);
+}
+
+// for updateEntry, we give plugins the opportunity to modify the entry specifically
+// inside of onBeforeUpdate
+async function updateEntry(id, data) {
+  let updatedValue = await _runPlugins("onBeforeUpdate", id, data);
+
+  if (updatedValue) db[id] = updatedValue;
+
+  db[id] = data;
+
+  await _runPlugins("onUpdate", id, data);
+  await _runPlugins("onAfterUpdate", id, data);
+}
+
+// for getEntry, we allow plugins to modify the entry in turn
+async function getEntry(id) {
+  let entry = db[id];
+
+  entry = await _runPlugins("onBeforeGetEntry", id, entry);
+  entry = await _runPlugins("onGetEntry", id, entry);
+  entry = await _runPlugins("onAfterGetEntry", id, entry);
+
+  return entry;
+}
+
+async function runApi(api, ...args) {
+  let [target, command] = api.split("/");
+
+  let plugin = plugins[target];
+  return await plugin.plugin[command](plugin.config, runApi, ...args);
+}
+
+async function _runPlugins(pluginFunctionType, ...args) {
+  for (const plugin of Object.values(plugins)) {
+    let pluginFunction = plugin.plugin[pluginFunctionType];
+    if (pluginFunction) {
+      await pluginFunction(plugin.config, runApi, ...args);
+    }
+  }
+}
+
+async function _loadPlugins() {
+  const configPlugins = config.get("plugins");
+  for (const configPlugin of configPlugins) {
+    const plugin = await import("./" + configPlugin.path);
+    plugins[configPlugin.name] = { plugin, config: configPlugin.config };
+  }
+}
+
+initialize();
---
+++ test-plugin.js
@@ -0,0 +1,52 @@
+
+
+export {
+  onBeforeInitialize,
+  onInitialize,
+  onAfterInitialize,
+
+  onBeforeUpdateEntry,
+  onUpdateEntry,
+  onAfterUpdateEntry,
+
+  onBeforeGetEntry,
+  onGetEntry,
+  onAfterGetEntry,
+}
+
+async function onBeforeInitialize(config, runApi) {
+  console.log('onBeforeInitialize');
+  await runApi("core/getEntry", "123");
+}
+
+async function onInitialize(config, runApi) {
+  console.log('onInitialize');
+}
+
+async function onAfterInitialize(config, runApi) {
+  console.log("onAfterInitialize");
+}
+
+async function onBeforeUpdateEntry(config, runApi, id, data) {
+  console.log("onBeforeUpdateEntry");
+}
+
+async function onUpdateEntry(config, runApi, id, data) {
+  console.log("onUpdateEntry");
+}
+
+async function onAfterUpdateEntry(config, runApi, id, data) {
+  console.log("onAfterUpdateEntry");
+}
+
+async function onBeforeGetEntry(config, runApi, id) {
+  console.log("onBeforeGetEntry");
+}
+
+async function onGetEntry(config, runApi, id) {
+  console.log("onGetEntry");
+}
+
+async function onAfterGetEntry(config, runApi, id) {
+  console.log("onAfterGetEntry");
+}
